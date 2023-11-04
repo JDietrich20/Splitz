2c920813dba75c5e99c6c41ad5032f6fc6fc158a
Julianna Dietrich <JDietrich@scu.edu>
2023-11-04 16:51:19 -0700

    Updated/implemented classes and functions for the Splitz model

--- ClassDeclarations.js
+++ ClassDeclarations.js
@@ -1,61 +1,175 @@
+
 class person{
-    //var: str name, int owes
-    constructor(name = "Hi", owes = 0){
+    constructor(name = "Hi"){
         this.name = name;
-        this.owes = owes;
     }
 }
 
+
 class item{
-    //var: str name, cost
-    constructor(name, cost){
+    constructor(name, cost=-1,numPeopleWhoOwe=-1){
             this.name = name;
             this.cost = cost;
+            this.numPeopleWhoOwe = numPeopleWhoOwe;
         }
 }
 
 class bill{
-    //var: arr of people: arr of items, hashtable, double total, double tip, double tax
-    constructor(people =[], items=[],peopleowe=[], total=0, tip=0, tax=0){
-            this.people = people;
+    //var: arr of items, hashtable, double total, double tip, double tax
+    constructor( items=[],peopleOwe=[], total=-1, subtotal=-1){
             this.items = items;
-            this.peopleowe=peopleowe;
+            this.peopleowe=peopleOwe;
             this.total = total;
-            this.tip = tip;
-            this.tax = tax;
+            this.subtotal = subtotal;
+    }
+
+    remove(item, array){
+        deletedVar = array.splice(item, 1);
+        return array;
+    }
+    
+    findIndexOfItem(item){
+        return peopleSplitting.indexOf(item);
     }
         
     //adding an item or person to the recpiet
-    additem(itemName, cost = 0){
-        this.items.push(new item(itemName, cost));
+    additem(itemName){
+        this.items.push(new item(itemName,-1));
     }
-    addperson(personName, personOwes=0){
-        this.people.push(new person(personName, personOwes));
-        this.peopleowe.push([])
+    addperson(personName){
+        this.peopleowe.set(new person(personName),[])
     }
 
-    //edits the input (cost, people, item)
-    editInput(itemName, newCost){}
-    editInput(itemName, people){
-        for (let personOwesWhat in peopleowe){
+    editItemCost(itemVar, newCost){
+        itemVar.cost = newCost; 
+    }
+
+    editPeopleWhoOweItem(itemVar, PeopleWhoOweItem){
+        itemVar.numPeopleWhoOwe = PeopleWhoOweItem.length;
+        for (let aPerson in peopleOwe.keys()){
+            if (aPerson in PeopleWhoOweItem){
+                if (!(itemVar in peopleOwe.get(aperson))){
+                    peopleOwe.set(aPerson,peopleOwe.get(aPerson).push(itemVar));
+                }
+            }else{
+                if (itemVar in peopleOwe.get(aPerson)){
+                    peopleOwe.set(aPerson,remove(itemVar,peopleOwe.get(aPerson)));
+                }
+            }
         }
     }
-    editInput(itemName, newName){}
 
-    //edits tax tip 
-    editTax(editedTax){this.tax = editedTax;}
-    editTip(editedTip){this.tip = editedTip;}
+    editInput(itemVar, newName){
+        itemVar.name = newName
+    }
+
+    //edits total 
     editTotal(editedTotal){this.total = editedTotal;} 
 
+
+
     //returns a hashmap of how much people owe
-    calculateSplit(){}
-    
-    //calculate total split for some Person
-    personSplitTotal(person){
-        //return (subtotal the person owes) + (tip/this.people) + (tax/this.people);
+    calculateSplit(){
+        //calc subtotal
+        calcualteSubtotal();
+
+        howMuchPeopleOwe = new Map();
+        for (let aPerson in peopleOwe.keys()){
+            howMuchPeopleOwe.set(aPerson,personSplitTotal(aPerson));
+        }
+        return howMuchPeopleOwe;
     }
+        
+
+
+    //calculate total split for some Person
+    personSplitTotal(aPerson){
+        let individualSubtotal = 0.0
+        for(let item in peopleOwe.get(aPerson)){
+            individualSubtotal += item.cost/item.numPeopleWhoOwe;
+        }
+        return  individualSubtotal/subtotal*total;
+    }
+
+    //sets the subtotal variable
+    calculateSubtotal(){
+        subtotal=0;
+        for (let anItem in items){
+            subtotal+=anItem.cost;
+        }
+    }
+
     
 }
 
 
+//testing ground
 
+//person class testing
+let person1 = new person();
+console.log(person1.name); //Hi
+console.log(person1.personOwes);//-1
+
+let person2 = new person("John Doe", 20);
+console.log(person2.name);//John Doe
+console.log(person2.personOwes);//20
+
+let person3 = new person("Becky");
+console.log(person3.name);//Becky
+console.log(person3.personOwes);//-1
+
+let person4 = new person(20);
+console.log(person4.name);//20
+console.log(person4.personOwes);//-1
+
+//map config and testing
+let a = new Map();
+a.set(person1,"125");
+a.set(person2,"2");
+person1.name = "21"
+console.log(a.get(person1));//125
+
+
+//Bill class
+//we are unable to remove specific indicies with pop() or shift() so
+//we must implement a function to do such 
+peopleSplitting = ["Josh", "Lily", "Daniel"];
+peopleSplitting.push("Ron", "George");
+peopleSplitting.push("Becca");
+peopleSplitting.pop();
+peopleSplitting.shift();
+recpiet = new bill(peopleSplitting);
+console.log(recpiet);
+
+
+
+
+/*
+//testing ground
+peopleSplitting = ["Josh", "Lily", "Daniel"];
+peopleSplitting.push("Ron", "George");
+peopleSplitting.push("Becca");
+peopleSplitting.pop();
+//peopleSplitting.shift();
+
+recpiet = new bill(peopleSplitting);
+console.log(recpiet);
+
+
+function remove(item, array){
+    deletedVar = array.splice(item, 1);
+    // console.log(item);
+    // console.log(deletedVar);
+    return array;
+}
+
+function findIndexOfItem(item){
+    return peopleSplitting.indexOf(item);
+}
+//console.log(findIndexOfItem("Lily"));
+
+remove(findIndexOfItem("Lily"), peopleSplitting);
+    
+console.log(recpiet);
+
+*/
